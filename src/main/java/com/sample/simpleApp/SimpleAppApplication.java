a76e659b769cf34f9919dfc0e14f867712416c67
Kasun madhushan wickramasinghe <119012876+KAXUN01@users.noreply.github.com>
2024-12-22 14:13:30 +0530

    add print name function

--- src/main/java/com/sample/simpleApp/SimpleAppApplication.java
+++ src/main/java/com/sample/simpleApp/SimpleAppApplication.java
@@ -1,13 +1,23 @@
-package com.sample.simpleApp;
+package com.example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class SimpleAppApplication {
+public class HelloWorldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
 
+    @RestController
+    public class HelloWorldController {
+
+        @GetMapping("/")
+        public String helloWorld() {
+            return "Hello, Kasun!";
+        }
+    }
 }
